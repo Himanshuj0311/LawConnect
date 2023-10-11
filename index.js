81f81442c750c45251244bb49a9b7239b62f4ace
Himanshuj0311 <himanshu.masai@gmail.com>
2023-10-11 23:13:39 +0530

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const {db}=require("./Config/db")
+const userrouter =require("./Routes/user")
+const cors = require("cors")
+
+const LawyerRoutes=require("./Routes/LawyerControllar")
+require("dotenv").config()
+const port = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+const { BookingRoute } = require("./Routes/booking.route")
+app.use("/user",userrouter)
+app.use("/getLawyer", LawyerRoutes)
+
+app.listen(port, () => {
+    try {
+        db()
+        console.log(`Server is running on port ${port}`)
+    } catch (error) {
+        console.error(error)
+    }
+
+})
+
+
